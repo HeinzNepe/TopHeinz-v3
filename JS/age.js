69e4641aa4b2f0d4f545bda7a835ae26746e395d
e-hennep <e-hennep@asvg.no>
2021-08-20 17:28:28 +0200

    age thing is automatic

---
+++ JS/age.js
@@ -0,0 +1,10 @@
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+console.log(calculate_age(new Date(2004, 11, 12)));
+
+document.getElementById("agejs").innerHTML = calculate_age(new Date(2004, 11, 12))
