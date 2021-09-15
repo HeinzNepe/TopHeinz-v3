a19f23fb9b1beb6784394db31ebeb102ec982b84
Henrik X <e-hennep@asvg.no>
2021-09-15 20:22:44 +0200

    did some small stuff

--- JS/index/index.js
+++ JS/index/index.js
@@ -1,19 +1,22 @@
-fetch("/JS/index/songs.json").then(response=>response.json()).then(data=>
-    {
-        //console.log(data)
-        for(const song of data) {
-            //console.log(song.name)
-            document.querySelector("#artist-input-js").innerHTML+=`
-                <div class="item-flex">
-					<a href="${song.link}" target="_blank">
-						<img alt="imageload failed" class="album-img" src="${song.image}">
-					</a>
-					<i>${song.name}</i>
-					<b>By ${song.artist}</b>
-					<small>${song.type}</small>
-				</div>
-            `
-            console.log(`${song.type}, ${song.name}, ${song.artist}`)
+
+//This is the JavaScript for inputting the music cards in index
+    fetch("/JS/index/songs.json").then(response=>response.json()).then(data=>
+        {
+            //console.log(data)
+            for(const song of data) {
+                //console.log(song.name)
+                document.querySelector("#artist-input-js").innerHTML+=`
+                    <div class="item-flex">
+                        <a href="${song.link}" target="_blank">
+                            <img alt="imageload failed" class="album-img" src="${song.image}">
+                        </a>
+                        <i>${song.name}</i>
+                        <b>By ${song.artist}</b>
+                        <small>${song.type}</small>
+                    </div>
+                `
+                console.log(`${song.type}, ${song.name}, ${song.artist}`)
+            }
         }
-    }
-)
+    )
+//To here
