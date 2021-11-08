a53f4acc22cf0a798575336c2008e377f6e255d2
Henrik X <e-hennep@asvg.no>
2021-11-08 13:23:36 +0100

    Added project showcase. project.html is ready for projects from utvikling, brukerstøtte and driftstøtte

--- JS/index/song.js
+++ JS/index/song.js
@@ -9,7 +9,7 @@ fetch("/JS/index/songs.json").then(response=>response.json()).then(data=>{
     for(const song of data) {
         if(song.id.toString()===songinfo) {
             document.querySelector("#more-songinfo").innerHTML=`
-                <div class="info-item-flex">  
+                <div class="info-album-flex">  
                     <img alt="imageload failed" class="info-album-img" src="${song.image}">
                     <i>${song.name}</i>
                     <b>By ${song.artist}</b>
--- JS/utvikling/projects.js
+++ JS/utvikling/projects.js
@@ -1,21 +1,29 @@
 const project = new URLSearchParams(window.location.search)
 
-
+// Code for the detailed site on each of the projects
 
 fetch("/JS/utvikling/projects.json").then(response=>response.json()).then(data=>{
-
-    console.log("help" + project.id)
+    
     const projectinfo =  project.get("pid")
 
     for(const project of data) {
         if(project.id.toString()===projectinfo) {
             document.querySelector("#project-input-js").innerHTML=`
-                    <div class="info-item-flex">
-                        <img alt="imageload failed" class="info-album-img" src="${project.image}">
+                    </br>
+                    <h>${project.name}</h>
+                    <div class="infocard-project-flex">
+                        <img alt="imageload failed" class="info-project-img" src="${project.image}">
                         <b>${project.name}</b>
                         <smaller>${project.start} til ${project.end}</smaller>
                         <smaller>${project.status}</smaller>
                     </div>
+                    </br>
+                    <h>A short description</h>
+                    </br>
+                    <p class="proj-description">${project.desc}</p>
+                    </br>
+                    <p>This project is ${project.status}</p>
+                    </br>
             `
         }
 
