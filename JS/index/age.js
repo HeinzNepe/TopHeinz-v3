3dd5d0130a748ce0c4eeb339051897e4fa488dfd
e-hennep <e-hennep@asvg.no>
2021-11-16 13:44:13 +0100

    fix the birthday counter being dumb dumb

--- JS/index/age.js
+++ JS/index/age.js
@@ -5,7 +5,7 @@ function calculate_age(dob) {
     return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
 
-console.log("calculate_age(new Date(2004, 11, 12))");
-console.log(calculate_age(new Date(2004, 11, 12)));
+console.log("calculate_age(new Date(2004, 10, 12))");
+console.log(calculate_age(new Date(2004, 10, 12)));
 
-document.getElementById("agejs").innerHTML = calculate_age(new Date(2004, 11, 12))
+document.getElementById("agejs").innerHTML = calculate_age(new Date(2004, 10, 12))
