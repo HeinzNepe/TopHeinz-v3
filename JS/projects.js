93b3d614a6595133198b50c1f140f4a536c386b0
e-hennep <e-hennep@asvg.no>
2021-12-19 00:15:22 +0100

    project loading works, need to fix not load unnessecary stuff from JSON

--- JS/projects.js
+++ JS/projects.js
@@ -1,32 +1,32 @@
-const project = new URLSearchParams(window.location.search)
+const projectl = new URLSearchParams(window.location.search);
 
-let a
-let b
-let c
+let a;
+let b;
+let c;
 
 // Code for the detailed site on each of the projects
-fetch("/JS/brukerstotte/projects.json").then(response=>response.json()).then(data=>{a = JSON.stringify(data);})
-fetch("/JS/driftstotte/projects.json").then(response=>response.json()).then(data=>{b = JSON.stringify(data);})
-fetch("/JS/utvikling/projects.json").then(response=>response.json()).then(data=>{c = JSON.stringify(data);})
-    
-const projectinfo =  project.get("pid")
+fetch("/JS/brukerstotte/projects.json").then(response=>response.json()).then(data=>{a = data});
+fetch("/JS/driftstotte/projects.json").then(response=>response.json()).then(data=>{b = data});
+fetch("/JS/utvikling/projects.json").then(response=>response.json()).then(data=>{c = data});
+
 let everything = [];
-var project;
+
 setTimeout(()=>{
 
 
 
-console.log(a)
-console.log(b)
-console.log(c)
-
-everything = everything.concat.apply([],[a,b,c]);
-console.log(everything)
+    everything =  a.concat(b.concat(c))
 
 
-    for(project of everything) {
-    if (project.id.toString() === projectinfo) {
-        document.querySelector("#project-input-js").innerHTML = `
+
+    console.log(everything)
+
+
+    for(let project of everything) {
+        console.log(project.id + "  " + projectl.get("pid"))
+        if (project.id.toString() === projectl.get("pid")) {
+            console.log("It works")
+            document.querySelector("#project-input-js").innerHTML = `
                 </br>
                 <h style="margin: 1em">${project.name}</h>
                 <div class="infocard-project-flex">
@@ -44,8 +44,7 @@ console.log(everything)
                     </br>
                     <b>Project info:</b>
                     </br>
-                    <a href="${project.form}">Feedback form</a>
-                    </br>
+                    <div id="feedback"></div>
                     <a href="${project.link}">Link</a>
                 </div>
                 <div style="margin: 1em">
@@ -55,8 +54,12 @@ console.log(everything)
                     </br>
                 </div>
         `
-    }
-}},100)
+        if("form" in everything) {document.querySelector("#feedback").innerHTML = `
+          <a href="${project.form}">Feedback form</a>
+          </br>
+        `}
+        }
+    }},100)
 
 
 
